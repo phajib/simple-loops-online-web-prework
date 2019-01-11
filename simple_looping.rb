89179a0ad970c27bc1375c076b3dfafc3b42ae80
H B <phajib@gmail.com>
2019-01-11 18:27:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
     loop do
+      count += 1
       puts "#{phrase}"
       if count == number_of_times
         break
